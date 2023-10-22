19fe35bd09b233efa49164bb6caf24f1b6508ba8
Dominik <dominik.roman@bluelemons.sk>
2023-10-22 23:11:08 +0200

    gramaticke chyby, seo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script setup>
-import UnderConstruction from "@/components/UnderConstruction.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import TitulkaComponent from "@/components/TitulkaComponent.vue";
 import PonukaComponent from "@/components/PonukaComponent.vue";
 import SluzbyComponent from "@/components/SluzbyComponent.vue";
 import BagerComponent from "@/components/BagerComponent.vue";
 import PracaComponent from "@/components/PracaComponent.vue";
+import {useMeta} from 'vue-meta';
+
+const {meta} = useMeta();
+
+// Nastavenie meta tagov pre SEO
+meta.value = {
+    title: 'PSAVP',
+    meta: [
+        {
+            name: 'description',
+            content: 'Prípravné stavebné a výkopové práce.',
+        },
+        {
+            name: 'keywords',
+            content: 'prípravné práce, pripravne prace, stavebné práce, výkopové práce, prípravné stavebné a výkopové práce, PSAVP, psavp, východné Slovensko, ' +
+                'bager, stavba, výkop, Trebišov, Michalovce, Vranov, Vranov nad Topľou, Košice, Košice a okolie, ' +
+                'Dávid Hrubovský, Hrubovský, Hrubovský Dávid, David Hrubovsky, Hrubovsky, Hrubovsky David' +
+                'stavebne prace, vykopove prace, pripravne stavebne a vykopove prace, vychodne slovensko, vykop, dvor,' +
+                'bazen, Trebisov, Vranov nad Toplou, trebisov, vranov nad toplou, kosice, Kosice, Kosice a okolie, kosice a okolie,' +
+                'vyrovnavanie dvora, ',
+        },
+        {
+            name: 'robots',
+            content: 'index',
+        },
+        // Ďalšie meta tagy podľa potreby
+    ],
+};
 </script>
 
 <template>
--- src/components/PonukaComponent.vue
+++ src/components/PonukaComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col">
         <div class="w-[25%] mx-auto mt-[5%] relative">
-            <img class="xs:max-w-[128px] xsm:max-w-[144px] sm:max-w-[174px] md:max-w-[204px] lg:max-w-[334px] xl:max-w-[404px]" src="@/assets/ponuka/kolecko.svg">
+            <img class="xs:max-w-[128px] xsm:max-w-[144px] sm:max-w-[174px] md:max-w-[204px] lg:max-w-[334px] xl:max-w-[404px]" src="@/assets/ponuka/kolecko.svg" alt="kolečko">
             <h1 class="font text-psavp-yellow xs:text-lg xsm:text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-6xl absolute bottom-[25%] xs:left-[9%] xsm:left-[9%] md:left-[8%] lg:left-[10%] xl:left-[7%]">PONÚKAME</h1>
         </div>
         <div class="flex xs:flex-col md:flex-row mx-auto mt-[5%]">
@@ -24,17 +24,17 @@ export default {
             karticky: [
                 {
                     nadpis: "Výkopové práce",
-                    body: [ 'Výkopy základov rodinných domov a iných stavieb.', 'Hlbinné výkopy.', 'Vyrovnávanie pozemkov.', 'Čistenie pozemkov od náletových drevín, koreňov stromov a stavebného odpadu.'],
+                    body: [ 'Výkopy základov rodinných domov a iných stavieb', 'Hlbinné výkopy', 'Vyrovnávanie pozemkov', 'Čistenie pozemkov od náletových drevín, koreňov stromov a stavebného odpadu'],
                     tmave: false,
                 },
                 {
                     nadpis: "Búracie práce",
-                    body: [ 'Obytných stavieb, dielní, kôlní, prístreškov a ďalších.', 'Pri búracích prácach zabezpečujeme aj odvoz materiálu.'],
+                    body: [ 'Obytných stavieb, dielní, kôlní, prístreškov a ďalších', 'Pri búracích prácach zabezpečujeme aj odvoz materiálu'],
                     tmave: true,
                 },
                 {
                     nadpis: "Doplnkové práce",
-                    body: [ 'Výstavba plotov a drobných stavieb.', 'Vytváranie parkovacích miest.', 'Prípravy pre výstavbu verejných komunikácii.'],
+                    body: [ 'Výstavba plotov a drobných stavieb.', 'Vytváranie parkovacích miest', 'Prípravy pre výstavbu verejných komunikácii'],
                     tmave: false,
                 },
             ]
